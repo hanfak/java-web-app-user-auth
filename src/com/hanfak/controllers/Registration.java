07e22b996466a47a3a114c8480350b219331c5a7
Hanif Fakira <fakira.work@gmail.com>
2016-11-20 19:10:49 +0000

    US2 extracted storing params to user to method

--- src/com/hanfak/controllers/Registration.java
+++ src/com/hanfak/controllers/Registration.java
@@ -9,46 +9,38 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.hanfak.models.User;
 
-/**
- * Servlet implementation class Registration
- */
 @WebServlet("/register")
 public class Registration extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private User newUser;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public Registration() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendRedirect("register.jsp");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String username = request.getParameter("username");
 		String email 	= request.getParameter("email");
 		String mobile 	= request.getParameter("mobile");
 		String password = request.getParameter("password");
 		
-		User newUser = new User();
-		newUser.setUsername(username);
-		newUser.setEmail(email);
-		newUser.setPassword(password);
-		newUser.setMobile(mobile);
+		newUser = new User();
+		storeParamsInUser(username, email, mobile, password);
 		
 		System.out.println(newUser.getPassword());
 
 		response.sendRedirect(request.getContextPath() + "/login");
+	}
+	
+	private void storeParamsInUser(String username, String email, String mobile, String password){
+		newUser.setUsername(username);
+		newUser.setEmail(email);
+		newUser.setPassword(password);
+		newUser.setMobile(mobile);
 	}
 
 }
